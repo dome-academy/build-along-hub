04af8783fa1caf5d44e578fce4c67c7758edb193
Emeka Allison <emekaallison4@gmail.com>
2024-10-20 19:03:40 +0100

    Fix: Week timing fix

--- src/weeklyItems.ts
+++ src/weeklyItems.ts
@@ -3,7 +3,7 @@ export const weeklyItems = [
     weekNumber: 0,
     startDate: "2024-10-13T00:00:00",
     deadlineDate: "2024-10-19T20:00:00",
-    lastDate: "2024-10-20T11:59:00",
+    lastDate: "2024-10-20T23:59:59",
     liveSessions: {
       monday: {
         date: "21 Oct 2024",
@@ -36,7 +36,7 @@ export const weeklyItems = [
     weekNumber: 1,
     startDate: "2024-10-21T00:00:00",
     deadlineDate: "2024-10-26T23:59:00",
-    lastDate: "2024-10-27T11:59:00",
+    lastDate: "2024-10-27T23:59:59",
     liveSessions: {
       monday: {
         date: "21 Oct 2024",
@@ -100,25 +100,30 @@ export const weeklyItems = [
     weekNumber: 2,
     startDate: "2024-10-28T00:00:00",
     deadlineDate: "2024-11-02T23:59:00",
-    lastDate: "2024-11-03T11:59:00",
+    lastDate: "2024-11-03T23:59:59",
     liveSessions: {
       monday: {
         date: "28 Oct 2024",
         time: "8PM",
-        link: "https://lu.ma/ewpdy1ny",
+        link: "https://lu.ma/civn4f2w",
       },
       wednesday: {
         date: "30 Oct 2024",
         time: "8PM",
-        link: "https://lu.ma/l6t4gxpl",
+        link: "https://lu.ma/uzg7eh9d",
       },
       friday: {
         date: "1 Nov 2024",
         time: "8PM",
-        link: "https://lu.ma/wik749nu",
+        link: "https://lu.ma/ph5cnwuh",
       },
     },
     resources: [
+      {
+        title:
+          "Simple Git Commands Every Developer Must Know [Dome Academy Bytes]",
+        link: "https://domeacademy.substack.com/p/simple-git-commands-every-developer",
+      },
       {
         title: "The Box Model [MDN Web Docs]",
         link: "https://developer.mozilla.org/en-US/docs/Learn/CSS/Building_blocks/The_box_model",
@@ -161,8 +166,8 @@ export const weeklyItems = [
   {
     weekNumber: 3,
     startDate: "2024-11-04T00:00:00",
-    deadlineDate: "2024-11-09T23:59:00",
-    lastDate: "2024-11-10T11:59:00",
+    deadlineDate: "2024-11-09T23:59:59",
+    lastDate: "2024-11-10T23:59:59",
     liveSessions: {
       monday: {
         date: "4 Nov 2024",
@@ -196,6 +201,129 @@ export const weeklyItems = [
       {
         title: "CSS Transitions [W3Schools]",
         link: "https://www.w3schools.com/css/css3_transitions.asp",
+      },
+      {
+        title: "Recordings",
+        link: "https://spit.sh",
+      },
+    ],
+    projects: {
+      capStoneUpdate: "https://spit.sh",
+      weekProject: "https://spit.sh",
+    },
+  },
+  // Week 4
+  {
+    weekNumber: 4,
+    startDate: "2024-11-11T00:00:00",
+    deadlineDate: "2024-11-16T23:59:00",
+    lastDate: "2024-11-17T23:59:59",
+    liveSessions: {
+      monday: {
+        date: "11 Nov 2024",
+        time: "8PM",
+        link: "https://lu.ma/7ck3uo9z",
+      },
+      wednesday: {
+        date: "13 Nov 2024",
+        time: "8PM",
+        link: "https://lu.ma/lfnx6im8",
+      },
+      friday: {
+        date: "15 Nov 2024",
+        time: "8PM",
+        link: "https://lu.ma/t5q5gouo",
+      },
+    },
+    resources: [
+      {
+        title: "What is Computer Programming? [freeCodeCamp]",
+        link: "https://www.freecodecamp.org/news/what-is-programming",
+      },
+      {
+        title: "A Beginner's Guide to Programming [Dome Academy Bytes]",
+        link: "https://domeacademy.substack.com/p/a-beginners-guide-to-programming",
+      },
+      {
+        title: "Introduction to JavaScript [Geeks for Geeks]",
+        link: "https://css-tricks.com/snippets/css/a-guide-to-flexbox/",
+      },
+      {
+        title:
+          "JavaScript Tutorial for Beginners: Learn JavaScript in 1 Hour [YouTube]",
+        link: "https://www.youtube.com/watch?v=W6NZfCO5SIk",
+      },
+      {
+        title: "Javascript in 100 seconds [YouTube]",
+        link: "https://www.youtube.com/watch?v=DHjqpvDnNGE",
+      },
+      {
+        title: "Control flow [web.dev]",
+        link: "https://web.dev/learn/javascript/control-flow",
+      },
+      {
+        title: "Control Statements in JavaScript [Geek for Geeks]",
+        link: "https://web.dev/learn/javascript/control-flow",
+      },
+      {
+        title: "Recordings",
+        link: "https://spit.sh",
+      },
+    ],
+    projects: {
+      capStoneUpdate: "https://spit.sh",
+      weekProject: "https://spit.sh",
+    },
+  },
+  // Week 5
+  {
+    weekNumber: 5,
+    startDate: "2024-11-18T00:00:00",
+    deadlineDate: "2024-11-23T23:59:59",
+    lastDate: "2024-11-24T23:59:59",
+    liveSessions: {
+      monday: {
+        date: "18 Nov 2024",
+        time: "8PM",
+        link: "https://lu.ma/7ck3uo9z",
+      },
+      wednesday: {
+        date: "20 Nov 2024",
+        time: "8PM",
+        link: "https://lu.ma/lfnx6im8",
+      },
+      friday: {
+        date: "22 Nov 2024",
+        time: "8PM",
+        link: "https://lu.ma/t5q5gouo",
+      },
+    },
+    resources: [
+      {
+        title: "JavaScript Functions [w3schools]",
+        link: "https://www.w3schools.com/js/js_functions.asp",
+      },
+      {
+        title:
+          "What are Functions in JavaScript? A Beginner's Guide [freeCodeCamp]",
+        link: "https://www.freecodecamp.org/news/what-are-functions-in-javascript-a-beginners-guide/",
+      },
+      {
+        title: "JavaScript Recursion [Programiz]",
+        link: "https://www.programiz.com/javascript/recursion",
+      },
+      {
+        title: "JavaScript Recursion [Programiz]",
+        link: "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Manipulating_documents",
+      },
+      {
+        title: "The JavaScript DOM explained in 5 minutes! 🌳 [YouTube]",
+        link: "https://www.youtube.com/watch?v=NO5kUNxGIu0",
+      },
+      {
+        title:
+          "JavaScript DOM Manipulation Mastery: A Comprehensive Guide [YouTube]",
+        link: "https://www.youtube.com/watch?v=BGkc6dKUZ84",
       },
       {
         title: "Recordings",
